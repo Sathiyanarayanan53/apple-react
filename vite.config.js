854b9068fd3ee0740ae0b43261575c1717297a13
sathiyanarayanan53 <sathyanarayanan5303@gmail.com>
2025-05-14 20:21:40 +0530

    Update base and homepage for new repository apple-react

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/apple-clone-react/', // Match your GitHub Pages path
+  base: '/apple-react/', // Match your GitHub Pages path
 })
